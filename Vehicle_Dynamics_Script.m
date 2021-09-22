423b06511d142d84d4fbee1bc487e4c65bcb0fb2
sanket4912 <89934231+sanket4912@users.noreply.github.com>
2021-09-22 22:17:25 +0530

    Script_for_VD

---
+++ Vehicle_Dynamics_Script.m
@@ -0,0 +1,9 @@
+Steering_Angle=Simulink.Parameter;
+Steering_Angle.Value=20;
+Terrain=Simulink.Parameter;
+Terrain.Value=-2*(pi/180);
+Pedalling_Force=Simulink.Parameter;
+Pedalling_Force.Value=50;
+Braking=Simulink.Parameter;
+Braking.Value=0;
+
